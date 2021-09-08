9ab801a7328ac6259c98c444b91f0bf65d8312c9
Monwar <monwar259@gmail.com>
2021-09-08 13:16:38 +0100

    Added recipe model, and displayed mock data on recipe list

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "This is a description",  "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F7782449.jpg"),
+    new Recipe("Test Recipe", "This is a description",  "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F7782449.jpg")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
